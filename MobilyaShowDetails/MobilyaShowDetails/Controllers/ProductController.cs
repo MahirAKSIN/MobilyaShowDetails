aafe17d91b2454bd2edb421f127a48d22e4d0c01
Mahir Aksin <98876295+MahirAKSIN@users.noreply.github.com>
2022-05-30 18:03:56 +0300

    FirstMvcProject

---
+++ MobilyaShowDetails/MobilyaShowDetails/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MobilyaShowDetails.Models;
+
+namespace MobilyaShowDetails.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            var products = Respository.AllProduct();
+            return View(products);
+        }
+        public IActionResult getProductDetail()
+        {
+            var products = Respository.AllProduct();
+            Product resultProduct = null;
+            int id = Convert.ToInt32(ControllerContext.RouteData.Values["ID"]);
+            foreach (var item in products)
+            {
+                if (item.ID == id)
+                {
+                    resultProduct = item;
+                    break;
+
+                }
+            }
+            ViewBag.Product = resultProduct;
+            return View();
+        }
+
+    }
+}
---
+++ MobilyaShowDetails/MobilyaShowDetails/Models/Respository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MobilyaShowDetails.Models
+{
+    public static class Respository
+    {
+        public static List<Product> AllProduct()
+        {
+
+            var products = new List<Product>
+            {
+                new Product{ID=540,ProductName="Sürgülü Dolap1",ProductDetail="Sürgülü Rayli Aynali Dolap 1",Price=4500},
+                new Product{ID=545,ProductName="Sürgülü Dolap1",ProductDetail="Sürgülü Rayli Aynali Dolap 2",Price=5500},
+                new Product{ID=640,ProductName="Sürgülü Dolap1",ProductDetail="Sürgülü Rayli Aynali Dolap 3",Price=6500},
+                new Product{ID=440,ProductName="Sürgülü Dolap1",ProductDetail="Sürgülü Rayli Aynali Dolap 4",Price=7500},
+                new Product{ID=510,ProductName="Sürgülü Dolap1",ProductDetail="Sürgülü Rayli Aynali Dolap 5",Price=8500},
+                new Product{ID=590,ProductName="Sürgülü Dolap1",ProductDetail="Sürgülü Rayli Aynali Dolap 6",Price=9500}
+
+            };
+            return products;
+        }
+    }
+}
